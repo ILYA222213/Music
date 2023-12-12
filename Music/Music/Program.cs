c917034fe2be7b148839273b643470c7de02e7f6
Сом <117897751+ILYA222213@users.noreply.github.com>
2023-12-12 15:24:11 +0300

    Update Program.cs

--- Music/Music/Program.cs
+++ Music/Music/Program.cs
@@ -3,6 +3,6 @@
 HttpClient client = new HttpClient();
 HttpResponseMessage response = await client.GetAsync("https://rur.hitmotop.com/get/music/20220726/SHAMAN_-_YA_RUSSKIJJ_74622610.mp3");
 byte[] data = response.Content.ReadAsByteArrayAsync().Result;
-File.WriteAllBytes("C:\\Users\\ilruz\\OneDrive\\Рабочий стол\\Дора-дура.mp3", data);
+File.WriteAllBytes("C:\\Users\\ilruz\\OneDrive\\Рабочий стол\\Я РУССКИЙ.mp3", data);
 
-Process.Start(new ProcessStartInfo { FileName = "C:\\Users\\ilruz\\OneDrive\\Рабочий стол\\Дора-дура.mp3", UseShellExecute = true });
+Process.Start(new ProcessStartInfo { FileName = "C:\\Users\\ilruz\\OneDrive\\Рабочий стол\\Я РУССКИЙ.mp3", UseShellExecute = true });
